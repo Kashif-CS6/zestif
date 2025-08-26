de9c30d8b4fbb48249262fdf021f2ada082966eb
Kashif-CS6 <kashifnabeel.cs@gmail.com>
2025-08-27 01:49:50 +0500

    added Ads Day#3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,23 @@ import Template from "./Template";
 import Hero from "./_components/LandingPage/Hero";
 import Menu from "./_components/LandingPage/Menu";
 import CategoryMenu from "./_components/LandingPage/CategoryMenu";
+import Image from "next/image";
 
 const Home = () => {
   return (
     <Template>
       <Hero />
       <Menu />
-      <CategoryMenu/>
+      <CategoryMenu />
+      <div className="rounded-[12px] border-[1px] mb-12 h-[661px] border-[#00000033]">
+        <Image
+          src={"/ad.jpg"}
+          width={1533}
+          height={661}
+          alt="Ad"
+          className="object-cover  h-full rounded-[12px]  "
+        />
+      </div>
     </Template>
   );
 };
