ba628b765ce33bbf6f2a19119c341677e5735b23
Kashif-CS6 <kashifnabeel.cs@gmail.com>
2025-08-28 17:49:33 +0500

    record banner completed DAY#4

--- src/app/_components/LandingPage/RecordBanner.tsx
+++ src/app/_components/LandingPage/RecordBanner.tsx
@@ -2,8 +2,29 @@ import React from "react";
 
 const RecordBanner = () => {
   return (
-    <div className="mb-10 h-[158px] rounded-[12px] bg-[#FC8A06]">
-     
+    <div className="mb-10 h-[158px] rounded-[12px] bg-[#FC8A06] flex items-center justify-between text-white py-6">
+      <div className="flex-1 flex flex-col items-center justify-center gap-2 h-full border-r border-white">
+        <h2 className="text-[54px] font-[300] leading-[38px]">546+</h2>
+        <h2 className="font-[700] text-[24px] leading-[38px]">
+          Registered Riders
+        </h2>
+      </div>
+      <div className="flex-1 flex flex-col items-center justify-center gap-2 h-full border-r border-white">
+        <h2 className="text-[54px] font-[300] leading-[38px]">789,900+</h2>
+        <h2 className="font-[700] text-[24px] leading-[38px]">
+          Orders Delivered
+        </h2>
+      </div>
+      <div className="flex-1 flex flex-col items-center justify-center gap-2 h-full border-r border-white">
+        <h2 className="text-[54px] font-[300] leading-[38px]">690+</h2>
+        <h2 className="font-[700] text-[24px] leading-[38px]">
+          Restaurants Partnered
+        </h2>
+      </div>
+      <div className="flex-1 flex flex-col items-center justify-center gap-2 h-full ">
+        <h2 className="text-[54px] font-[300] leading-[38px]">17,457+</h2>
+        <h2 className="font-[700] text-[24px] leading-[38px]">Food items</h2>
+      </div>
     </div>
   );
 };
