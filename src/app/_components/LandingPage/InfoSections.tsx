a1a9cd178a18fa8e62926f06e4001178c561ed69
Kashif-CS6 <kashifnabeel.cs@gmail.com>
2025-11-03 15:54:02 +0500

    pushing code or zestif

--- src/app/_components/LandingPage/InfoSections.tsx
+++ src/app/_components/LandingPage/InfoSections.tsx
@@ -2,37 +2,37 @@ import React from "react";
 
 const InfoSections = () => {
   return (
-    <div className="mb-10 h-[882px] rounded-[12px] bg-[#D9D9D9]  flex flex-col items-center justify-center gap-16">
-      <div className="flex items-center justify-between w-[1290px]">
+    <div className="mb-10 py-10 rounded-[12px] bg-[#D9D9D9]  flex flex-col items-center justify-center gap-10 px-10">
+      <div className="flex items-center justify-between w-full ">
         <h1 className="leading-[100%] text-3xl font-[600]">
           Know more about us!
         </h1>
         <ul className="flex items-center">
           <li>
-            <button className="text-[16px] font-[400] leading-[100%] h-[61px] focus:font-[600] focus:rounded-[120px] focus:border-[1px] focus:border-[#FC8A06] focus:text-[#FC8A06] px-7">
+            <button className="text-sm font-[400] leading-[100%] py-3 focus:font-[600] focus:rounded-[120px] focus:border-[1px] focus:border-[#FC8A06] focus:text-[#FC8A06] px-4">
               Frequest Questions
             </button>
           </li>
           <li>
-            <button className="text-[16px] font-[400] leading-[100%] h-[61px] focus:font-[600] focus:rounded-[120px] focus:border-[1px] focus:border-[#FC8A06] focus:text-[#FC8A06] px-7">
+            <button className="text-sm font-[400] leading-[100%] py-3 focus:font-[600] focus:rounded-[120px] focus:border-[1px] focus:border-[#FC8A06] focus:text-[#FC8A06] px-4">
               Who we are?
             </button>
           </li>
           <li>
             {" "}
-            <button className="text-[16px] font-[400] leading-[100%] h-[61px] focus:font-[600] focus:rounded-[120px] focus:border-[1px] focus:border-[#FC8A06] focus:text-[#FC8A06] px-7">
+            <button className="text-sm font-[400] leading-[100%] py-3 focus:font-[600] focus:rounded-[120px] focus:border-[1px] focus:border-[#FC8A06] focus:text-[#FC8A06] px-4">
               Partner Program
             </button>
           </li>
           <li>
             {" "}
-            <button className="text-[16px] font-[400] leading-[100%] h-[61px] focus:font-[600] focus:rounded-[120px] focus:border-[1px] focus:border-[#FC8A06] focus:text-[#FC8A06] px-7">
+            <button className="text-sm font-[400] leading-[100%] py-3 focus:font-[600] focus:rounded-[120px] focus:border-[1px] focus:border-[#FC8A06] focus:text-[#FC8A06] px-4">
               Help & Support
             </button>
           </li>
         </ul>
       </div>
-      <div className="h-[526px] bg-[#03081F] rounded-[12px] w-[1290px]"></div>
+      <div className=" bg-[#03081F] rounded-[12px] "></div>
     </div>
   );
 };
--- src/app/_components/LandingPage/JoinOptionCard.tsx
+++ src/app/_components/LandingPage/JoinOptionCard.tsx
@@ -15,22 +15,22 @@ const JoinOptionCard: FC<JoinOptionCardTypes> = ({ item }) => {
   return (
     <div
       style={{ backgroundImage: `url(/chickens.jpg)` }}
-      className="w-[754px] h-[425px] rounded-[12px] bg-cover relative flex items-end"
+      className="w-1/2 h-[310px] rounded-[12px] bg-cover relative flex items-end"
     >
       <div className="absolute rounded-[12px] inset-0 bg-[linear-gradient(247.7deg,_rgba(255,_255,_255,_0)_0.94%,_rgba(3,_8,_31,_0.19)_75.68%,_rgba(3,_8,_31,_0.89)_95.9%)]"></div>
-      <div className="w-[288px] h-[63px] rounded-b-[12px] bg-white flex items-center justify-center absolute top-0 left-14">
-        <h1 className="font-[700] text-[18px] text-[#03081F]">{item.tag}</h1>
+      <div className="px-10 py-3 rounded-b-[12px] bg-white flex items-center justify-center absolute top-0 left-14">
+        <h1 className="font-[700] text-sm text-[#03081F]">{item.tag}</h1>
       </div>
       <div className="p-12 flex flex-col gap-4 z-10">
         <div className="flex flex-col gap-1">
-          <h1 className="text-[#FC8A06] text-[18px] font-[500] leading-[100%]">
+          <h1 className="text-[#FC8A06] text-sm font-[500] leading-[100%]">
             {item.subheading}
           </h1>
-          <h1 className="text-[44px] font-[700] leading-[100%] text-white">
+          <h1 className="text-2xl font-[700] leading-[100%] text-white">
             {item.heading}
           </h1>
         </div>
-        <button className="w-[205px] h-[52px] rounded-[120px] bg-[#FC8A06] text-[18px] font-[500] text-white">
+        <button className="px-10 py-3 rounded-[120px] bg-[#FC8A06] text-sm font-[500] text-white">
           {item.buttontitle}
         </button>
       </div>
